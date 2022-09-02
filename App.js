70ecf02fc2275f79f72bbd371f1c066c307f65f9
jussimar <jussimar@jussimarleal.com.br>
2022-09-02 11:27:51 -0300

    criando estilos do app e criando componente tempo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { React } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-
+import Tempo from './components/Tempo';
 export default function App() {
   return (
     <View style={styles.container}>
@@ -19,6 +19,9 @@ export default function App() {
                 <Text style={styles.textoBotao}>Buscar</Text>
             </TouchableOpacity>
         </View>
+        <View style={styles.bloco}>
+            <Tempo />
+        </View>
     </View>
   );
 }
@@ -26,6 +29,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:30
   },
+  bloco:{
+    marginTop:30,
+    width:'80%',
+    marginLeft:'10%'
+  },
+  label:{
+    fontSize:20
+  },
+  titulo:{
+    fontSize:30,
+    textAlign:'center'
+  },
+  input:{
+    borderBottomWidth:2,
+    fontSize:20
+  },
+  botao:{
+    backgroundColor:"#000",
+  },
+  textoBotao:{
+    fontSize:20,
+    color:"#FFF",
+    textAlign:'center'
+  }
 });
+
+//https://api.hgbrasil.com/weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,description&key=4daebe67&city_name=Peruibe,SP
---
+++ components/Tempo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+export default function components() {
+ return (
+   <>
+        <Text style={styles.titulo}>Dados do Tempo para hoje</Text>
+        <Text style={styles.texto}>Máx: </Text>
+        <Text style={styles.texto}>Mín: </Text>
+        <Text style={styles.texto}>Descrição: </Text>
+
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+    titulo:{
+        fontSize:25
+    },
+    texto:{
+        fontSize:20
+    }
+})
