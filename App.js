294ed0204ac73a98a5efbddeafffd41df3fd322e
BrunaElizabeth1 <brunaelizabethgomescordeiro@gmail.com>
2022-09-23 10:56:47 -0300

    criando método de busca de tempo no app.js e sincronizando com o componente tempo

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import { React } from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Tempo from './components/Tempo';
+import Api from './components/Api';
+
 export default function App() {
+const [dados,setDados] = useState("");
+const [cidade,setCidade] = useState("");
+
+    async function carregaTempo(){
+      const response = await Api.get(`weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,description&key=56cfd9ae&city_name=${cidade},SP`);
+      setDados(response.data.forecast[0]);
+    }
+
   return (
+    
     <View style={styles.container}>
         <View style={styles.bloco}>
           <Text style={styles.titulo}>Previsão do Tempo</Text>
@@ -11,16 +22,16 @@ export default function App() {
           <Text style={styles.label}>Cidade: </Text>
           <TextInput 
             placeholder='digite aqui a cidade ...'
-            style={styles.input}
+            style={styles.input} value={cidade} onChangeText = {(value)=>setCidade(value)}
           />
         </View>
         <View style={styles.bloco}>
-            <TouchableOpacity style={styles.botao}>
+            <TouchableOpacity style={styles.botao} onPress={carregaTempo}>
                 <Text style={styles.textoBotao}>Buscar</Text>
             </TouchableOpacity>
         </View>
         <View style={styles.bloco}>
-            <Tempo />
+            <Tempo previsao={dados}/>
         </View>
     </View>
   );
@@ -56,4 +67,4 @@ const styles = StyleSheet.create({
   }
 });
 
-//https://api.hgbrasil.com/weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,description&key=4daebe67&city_name=Peruibe,SP
+//https://api.hgbrasil.com/weather?array_limit=1&fields=only_results,temp,city_name,forecast,max,min,date,description&key=56cfd9ae&city_name=Peruibe,SP
--- components/Tempo.js
+++ components/Tempo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
-export default function components() {
+export default function Tempo(props) {
  return (
    <>
         <Text style={styles.titulo}>Dados do Tempo para hoje</Text>
-        <Text style={styles.texto}>Máx: </Text>
-        <Text style={styles.texto}>Mín: </Text>
-        <Text style={styles.texto}>Descrição: </Text>
+        <Text style={styles.texto}>Máx: {props.previsao.max}</Text>
+        <Text style={styles.texto}>Mín: {props.previsao.min}</Text>
+        <Text style={styles.texto}>Descrição: {props.previsao.description}</Text>
 
    </>
   );
